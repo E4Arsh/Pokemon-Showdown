94d9a9152861001ca9e59cf6a7555487b43f4476
E4Arsh <arshrulesrs@gmail.com>
2014-01-08 18:12:07 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -10,8 +10,7 @@ exports.proxyip = false;
 
 // Pokemon of the Day - put a pokemon's name here to make it Pokemon of the Day
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
-//   in every Random Battle team.wer
-
+//   in every Random Battle team.
 exports.potd = '';
 
 // crash guard - write errors to log file instead of crashing
@@ -28,38 +27,38 @@ exports.loginserver = 'http://play.pokemonshowdown.com/';
 exports.loginserverkeyalgo = "RSA-SHA1";
 exports.loginserverpublickeyid = 2;
 exports.loginserverpublickey = "-----BEGIN RSA PUBLIC KEY-----\n" +
-	"MIICCgKCAgEAtFldA2rTCsPgqsp1odoH9vwhf5+QGIlOJO7STyY73W2+io33cV7t\n" +
-	"ReNuzs75YBkZ3pWoDn2be0eb2UqO8dM3xN419FdHNORQ897K9ogoeSbLNQwyA7XB\n" +
-	"N/wpAg9NpNu00wce2zi3/+4M/2H+9vlv2/POOj1epi6cD5hjVnAuKsuoGaDcByg2\n" +
-	"EOullPh/00TkEkcyYtaBknZpED0lt/4ekw16mjHKcbo9uFiw+tu5vv7DXOkfciW+\n" +
-	"9ApyYbNksC/TbDIvJ2RjzR9G33CPE+8J+XbS7U1jPvdFragCenz+B3AiGcPZwT66\n" +
-	"dvHAOYRus/w5ELswOVX/HvHUb/GRrh4blXWUDn4KpjqtlwqY4H2oa+h9tEENCk8T\n" +
-	"BWmv3gzGBM5QcehNsyEi9+1RUAmknqJW0QOC+kifbjbo/qtlzzlSvtbr4MwghCFe\n" +
-	"1EfezeNAtqwvICznq8ebsGETyPSqI7fSbpmVULkKbebSDw6kqDnQso3iLjSX9K9C\n" +
-	"0rwxwalCs/YzgX9Eq4jdx6yAHd7FNGEx4iu8qM78c7GKCisygZxF8kd0B7V7a5UO\n" +
-	"wdlWIlTxJ2dfCnnJBFEt/wDsL54q8KmGbzOTvRq5uz/tMvs6ycgLVgA9r1xmVU+1\n" +
-	"6lMr2wdSzyG7l3X3q1XyQ/CT5IP4unFs5HKpG31skxlfXv5a7KW5AfsCAwEAAQ==\n" +
-	"-----END RSA PUBLIC KEY-----\n";
+        "MIICCgKCAgEAtFldA2rTCsPgqsp1odoH9vwhf5+QGIlOJO7STyY73W2+io33cV7t\n" +
+        "ReNuzs75YBkZ3pWoDn2be0eb2UqO8dM3xN419FdHNORQ897K9ogoeSbLNQwyA7XB\n" +
+        "N/wpAg9NpNu00wce2zi3/+4M/2H+9vlv2/POOj1epi6cD5hjVnAuKsuoGaDcByg2\n" +
+        "EOullPh/00TkEkcyYtaBknZpED0lt/4ekw16mjHKcbo9uFiw+tu5vv7DXOkfciW+\n" +
+        "9ApyYbNksC/TbDIvJ2RjzR9G33CPE+8J+XbS7U1jPvdFragCenz+B3AiGcPZwT66\n" +
+        "dvHAOYRus/w5ELswOVX/HvHUb/GRrh4blXWUDn4KpjqtlwqY4H2oa+h9tEENCk8T\n" +
+        "BWmv3gzGBM5QcehNsyEi9+1RUAmknqJW0QOC+kifbjbo/qtlzzlSvtbr4MwghCFe\n" +
+        "1EfezeNAtqwvICznq8ebsGETyPSqI7fSbpmVULkKbebSDw6kqDnQso3iLjSX9K9C\n" +
+        "0rwxwalCs/YzgX9Eq4jdx6yAHd7FNGEx4iu8qM78c7GKCisygZxF8kd0B7V7a5UO\n" +
+        "wdlWIlTxJ2dfCnnJBFEt/wDsL54q8KmGbzOTvRq5uz/tMvs6ycgLVgA9r1xmVU+1\n" +
+        "6lMr2wdSzyG7l3X3q1XyQ/CT5IP4unFs5HKpG31skxlfXv5a7KW5AfsCAwEAAQ==\n" +
+        "-----END RSA PUBLIC KEY-----\n";
 
 // crashguardemail - if the server has been running for more than an hour
 // and crashes, send an email using these settings, rather than locking down
 // the server. Uncomment this definition if you wan to use this feature;
 // otherwise, all crashes will lock down the server.
 /**exports.crashguardemail = {
-	transport: 'SMTP',
-	options: {
-		host: 'mail.example.com',
-		port: 465,
-		secureConnection: true,
-		maxConnections: 1,
-		auth: {
-			user: 'example@domain.com',
-			pass: 'password'
-		}
-	},
-	from: 'crashlogger@example.com',
-	to: 'admin@example.com',
-	subject: 'Pokemon Showdown has crashed!'
+        transport: 'SMTP',
+        options: {
+                host: 'mail.example.com',
+                port: 465,
+                secureConnection: true,
+                maxConnections: 1,
+                auth: {
+                        user: 'example@domain.com',
+                        pass: 'password'
+                }
+        },
+        from: 'crashlogger@example.com',
+        to: 'admin@example.com',
+        subject: 'Pokemon Showdown has crashed!'
 };**/
 
 // report joins and leaves - shows messages like "<USERNAME> joined"
@@ -67,25 +66,27 @@ exports.loginserverpublickey = "-----BEGIN RSA PUBLIC KEY-----\n" +
 //   be more than one line of messages.
 //   This feature can lag larger servers - turn this off if your server is
 //   getting more than 80 or so users.
-exports.reportjoins = false;
+exports.reportjoins = true;
 
 // report battles - shows messages like "OU battle started" in the lobby
 //   This feature can lag larger servers - turn this off if your server is
 //   getting more than 160 or so users.
-exports.reportbattles = false;
+exports.reportbattles = true;
 
-// moderated chat - prevent unregistered, unvoiced users from speaking
-//   This should only be enabled temporarily, when you're dealing with
-//   huge influxes of spammy users.
-exports.modchat = false;
+// moderated chat - prevent unvoiced users from speaking
+//   This should only be enabled in special situations, such as temporarily
+//   when you're dealing with huge influxes of spammy users.
+exports.chatmodchat = false;
+exports.battlemodchat = false;
+exports.pmmodchat = false;
 
-// backdoor - allows Zarel and his authorised Pokemon Showdown development
-//            staff to provide tech support for your server
-//   This backdoor gives Zarel (and development staff approved by him) admin
-//   access to your server, which allows him to provide tech support. This
+// backdoor - allows Pokemon Showdown system operators to provide technical
+//            support for your server
+//   This backdoor gives system operators (such as Zarel) console admin
+//   access to your server, which allow them to provide tech support. This
 //   can be useful in a variety of situations: if an attacker attacks your
 //   server and you are not online, if you need help setting up your server,
-//   etc. It is a backdoor, though, so if you do not trust Zarel you should
+//   etc. If you do not trust Pokemon Showdown with admin access, you should
 //   disable this feature.
 exports.backdoor = true;
 
@@ -99,8 +100,7 @@ exports.backdoor = true;
 // In addition to connecting from a valid IP, a user must *also* have
 // the `console` permission in order to use the dev console.
 // Setting this to an empty array ([]) will disable the dev console.
-exports.consoleips = ['blakjack', 'e4arsh', 'jackdaw'];
-exports.ktnDev = ['blakjack', 'e4rsh', 'jackdaw']; 
+exports.consoleips = ['127.0.0.1'];
 
 // Whether to watch the config file for changes. If this is enabled,
 // then the config.js file will be reloaded when it is changed.
@@ -109,7 +109,7 @@ exports.ktnDev = ['blakjack', 'e4rsh', 'jackdaw'];
 exports.watchconfig = true;
 
 // logchat - whether to log chat rooms.
-exports.logchat = true;
+exports.logchat = false;
 
 // loguserstats - how often (in milliseconds) to write user stats to the
 // lobby log. This has no effect if `logchat` is disabled.
@@ -136,44 +136,46 @@ exports.herokuhack = false;
 // Your server *must* be registered in order for your custom avatars to be
 // displayed in the client.
 exports.customavatars = {
-	//'userid': 'customavatar.png'
-	  'blakjack': 'blakjack.png',
-	  'skarr': 'skarr.gif',
-	  'omegahunter': 'zero.png',
-	  'frankentein': 'frankentein.png',
-	  'sooperpooper': 'sooperpooper.gif',
-	  'masterblazing': 'masterblazing.png',
-	  'surfersunite': 'gymleadersurfing.gif',
-	  'queenofdubstep': 'queenofdubstep.gif',
-	  'championcorper': 'championcorper.gif',
-	  'drascension': 'ncrypt.gif',
-	  'boo118': 'boo118.gif',
-	  'achampaero': 'achampaero.gif',
-	  'championonyxe': 'championonyxe.png',
-	  'achampdarkoath': 'achampdarkoath.png',
-	  'arshmalik': 'blakjack.png',
-	  'kingarani': 'kingarani.gif',
-	  'vaderdarkside': 'vaderdarkside.gif',
-	  'kupo': 'kupo.png',
-	  'unovachampionn': 'ucn.gif',
-	  'jackdaw': 'jackdaw.gif',
-	  'chaosherosilver': 'silver.gif',
-	  'e4photon': 'photon.gif',
-	  'checkm8t': 'photon.gif',
-	  'calvinz': 'calvinz.png',
-	  '10messi': '10messi.gif',
-	  'dis': 'dis.png',
-	  'aelitestorm': 'storm.gif',
-	  'shirayuri': 'okami95.png',
-	  'zarif': 'zarif.png',
-	  'enzarif': 'zarfalienbarf.gif',
-	  'arjunb': 'arjunb.gif',
-	  'ryuuga': 'ry.png',
+        //'userid': 'customavatar.png'
+        'blakjack': 'blakjack.png',
+          'skarr': 'skarr.gif',
+          'omegahunter': 'zero.png',
+          'frankentein': 'frankentein.png',
+          'sooperpooper': 'sooperpooper.gif',
+          'masterblazing': 'masterblazing.png',
+          'surfersunite': 'gymleadersurfing.gif',
+          'queenofdubstep': 'queenofdubstep.gif',
+          'championcorper': 'championcorper.gif',
+          'drascension': 'ncrypt.gif',
+          'boo118': 'boo118.gif',
+          'achampaero': 'achampaero.gif',
+          'championonyxe': 'championonyxe.png',
+          'achampdarkoath': 'achampdarkoath.png',
+          'arshmalik': 'blakjack.png',
+          'kingarani': 'kingarani.gif',
+          'vaderdarkside': 'vaderdarkside.gif',
+          'kupo': 'kupo.png',
+          'unovachampionn': 'ucn.gif',
+          'jackdaw': 'jackdaw.gif',
+          'chaosherosilver': 'silver.gif',
+          'e4photon': 'photon.gif',
+          'checkm8t': 'photon.gif',
+          'calvinz': 'calvinz.png',
+          '10messi': '10messi.gif',
+          'dis': 'dis.png',
+          'aelitestorm': 'storm.gif',
+          'shirayuri': 'okami95.png',
+          'zarif': 'zarif.png',
+          'enzarif': 'zarfalienbarf.gif',
+          'arjunb': 'arjunb.gif',
+          'ryuuga': 'ry.png',
           'astrelegit': 'astre.png',
           'e6mac': 'mac.png',
           'e4aknolan': 'aknolan.jpg',
           'xxhoodedxx': 'hood.jpg',
-          'spoonfullofmeat' :'sfom.png'
+          'spoonfullofmeat' :'sfom.png',
+
+
 };
 
 // appealurl - specify a URL containing information on how users can appeal
@@ -220,89 +222,95 @@ exports.appealurl = '';
 //     - potd: Set PotD.
 //     - forcewin: /forcewin command.
 //     - battlemessage: /a command.
-exports.groupsranking = [' ', '+', '%', '@', '#', '&', '~'];
+exports.groupsranking = [' ', '+', '\u2605', '%', '@', '#', '&', '~'];
 exports.groups = {
-	'~': {
-		id: "admin",
-		name: "Administrator",
-		root: true,
-		rank: 6
-	},
-	'&': {
-		id: "leader",
-		name: "Leader",
-		inherit: '@',
-		jurisdiction: '@u',
-		promote: 'u',
-		forcewin: true,
-		declare: true,
-		hide: true,
-		modchatall: true,
-		rangeban: true,
-		potd: true,
-		disableladder: true,
-		forcejoin: true,
-		permaban: true,
-		makeroom: true,
-		rank: 5
-	},
-	'#': {
-		id: "owner",
-		name: "Room Owner",
-		inherit: '@',
-		jurisdiction: 'u',
-		roomdriver: true,
-		roomoperator: true,
-		roomleader: true,
-		roommod: true,
-		roomonly: true,
-		declare: true,
-		modchatall: true,
-		rank: 4
-	},
-	'@': {
-		id: "mod",
-		name: "Moderator",
-		inherit: '%',
-		jurisdiction: 'u',
-		ban: true,
-		modchat: true,
-		forcerename: true,
-		ip: true,
-		alts: '@u',
-		rank: 3
-	},
-	'%': {
-		id: "driver",
-		name: "Driver",
-		inherit: '+',
-		jurisdiction: 'u',
-		announce: true,
-		warn: true,
-		kick: true,
-		mute: true,
-		lock: true,
-		forcerename: true,
-		timer: true,
-		modlog: true,
-		alts: '%u',
-		bypassblocks: 'u%@&~',
-		receiveauthmessages: true,
-		roomvoice: true,
-		rank: 2
-
-	},
-	'+': {
-		id: "voice",
-		name: "Voice",
-		inherit: ' ',
-		broadcast: true,
-		rank: 1
-	},
-	' ': {
-		ip: 's',
-		alts: 's',
-		rank: 0
-	}
+        '~': {
+                id: "admin",
+                name: "Administrator",
+                root: true,
+                globalonly: true,
+                gdeclare: true,
+                rank: 7
+        },
+        '&': {
+                id: "leader",
+                name: "Leader",
+                inherit: '@',
+                jurisdiction: '@u',
+                promote: 'u',
+                forcewin: true,
+                declare: true,
+                modchatall: true,
+                rangeban: true,
+                potd: true,
+                disableladder: true,
+                globalonly: true,
+                rank: 6
+        },
+        '#': {
+                id: "owner",
+                name: "Room Owner",
+                inherit: '@',
+                jurisdiction: 'u',
+                roommod: true,
+                roomdriver: true,
+                declare: true,
+                modchatall: true,
+                roomonly: true,
+                rank: 5
+        },
+        '@': {
+                id: "mod",
+                name: "Moderator",
+                inherit: '%',
+                jurisdiction: 'u',
+                ban: true,
+                modchat: true,
+                roomvoice: true,
+                forcerename: true,
+                ip: true,
+                alts: '@u',
+                rank: 4
+        },
+        '%': {
+                id: "driver",
+                name: "Driver",
+                inherit: '+',
+                jurisdiction: 'u',
+                announce: true,
+                warn: true,
+                kick: true,
+                mute: true,
+                lock: true,
+                forcerename: true,
+                timer: true,
+                modlog: true,
+                alts: '%u',
+                bypassblocks: 'u%@&~',
+                receiveauthmessages: true,
+                rank: 3
+        },
+        '\u2605': {
+                id: "player",
+                name: "Player",
+                inherit: '+',
+                roomvoice: true,
+                modchat: true,
+                roomonly: true,
+                privateroom: true,
+                rank: 2
+        },
+        '+': {
+                id: "voice",
+                name: "Voice",
+                inherit: ' ',
+                broadcast: true,
+                joinbattle: true,
+                rank: 1
+        },
+        ' ': {
+                ip: 's',
+                alts: 's',
+                rank: 0
+        }
 };
-
